4686e32739158c08398dfdcb68a6cae3a37cec6a
DC-21 <cholahkuboko@gmail.com>
2023-05-12 16:34:22 +0200

    Commit message

--- profile/src/components/Header.jsx
+++ profile/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import logo from '../images/png/logo-color.png'
+import { FaSearch } from 'react-icons/fa';
+
 
 const Header = () => {
   return (
@@ -11,8 +13,12 @@ const Header = () => {
               <img src={logo} alt="Logo" className="h-20 w-20 object-contain"/>
             </a>
             <div className="hidden md:block justify-center">
-              <div className="ml-10 flex  space-x-4 items-center">
-              <input type="text" placeholder="Search" className="border border-gray-400 rounded py-2 px-4 mr-2"/>
+            <div className="ml-10 flex  space-x-4 items-center">
+            <input type="text" placeholder="Search" className="border border-gray-400 rounded py-2 px-4 mr-2" />
+            <button type="submit" className="absolute right-0 top-0 mt-3 mr-4">
+            <FaSearch />
+            </button>
+
                 <a href="#" className="text-gray-300  hover:bg-blue-100 hover:text-black text-large font-medium py-2 px-4 border border-gray-400 rounded shadow">Home</a>
                 <a href="#" className="text-gray-300  hover:bg-blue-100 hover:text-black text-large font-medium py-2 px-4 border border-gray-400 rounded shadow">About</a>
                 <a href="#" className="text-gray-300  hover:bg-blue-100 hover:text-black text-large font-medium py-2 px-4 border border-gray-400 rounded shadow">Services</a>
