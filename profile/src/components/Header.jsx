11d40cf4678035fe50b58c3ce593310da18ca47f
DC-21 <cholahkuboko@gmail.com>
2023-05-12 17:50:15 +0200

    Commit message

--- profile/src/components/Header.jsx
+++ profile/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import logo from '../images/png/logo-color.png'
-import { FaSearch } from 'react-icons/fa';
+import { BsSearch } from 'react-icons/bs';
 
 
 const Header = () => {
@@ -16,7 +16,7 @@ const Header = () => {
             <div className="ml-10 flex  space-x-4 items-center">
             <input type="text" placeholder="Search" className="border border-gray-400 rounded py-2 px-4 mr-2" />
             <button type="submit" className="absolute right-0 top-0 mt-3 mr-4">
-            <FaSearch />
+            <BsSearch />
             </button>
 
                 <a href="#" className="text-gray-300  hover:bg-blue-100 hover:text-black text-large font-medium py-2 px-4 border border-gray-400 rounded shadow">Home</a>
