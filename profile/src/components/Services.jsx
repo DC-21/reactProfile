0395f7a51042cdc5884dbef955375f2c29683d77
DC-21 <cholahkuboko@gmail.com>
2023-05-14 12:06:36 +0200

    commit message

--- profile/src/components/Services.jsx
+++ profile/src/components/Services.jsx
@@ -13,7 +13,7 @@ const Services = () => {
         <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Our Services</h1>
         <div className="h-1 w-40 bg-indigo-500 rounded"></div>
       </div>
-      <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Kindly checkout our Services</p>
+      <p className="lg:w-1/2 w-full leading-relaxed text-gray-700">Kindly checkout our Services</p>
     </div>
     <div className="flex flex-wrap -m-4">
       <div className="xl:w-1/4 md:w-1/2 p-4">
@@ -21,7 +21,7 @@ const Services = () => {
           <img className="h-40 rounded w-full object-cover object-center mb-6" alt="hero" src={hero}/>
           <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">Web Devlopment</h3>
           <h2 className="text-lg text-gray-900 font-medium title-font mb-4">FrontEnd</h2>
-          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <p className="leading-relaxed text-base">As an excellent frontend developer i craft intuitive, performant interfaces with precision based on client's criteria.</p>
         </div>
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
@@ -29,7 +29,7 @@ const Services = () => {
           <img className="h-40 rounded w-full object-cover object-center mb-6" alt="mac" src={mac}/>
           <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">CyberSecurity</h3>
           <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Web and Server pentesting</h2>
-          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <p className="leading-relaxed text-base">As an excellent web and server pentester finds and secures vulnerabilities effectively.</p>
         </div>
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
