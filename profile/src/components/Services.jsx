fd9016de3d92073fd580dfe1dcdca3af6350d389
DC-21 <cholahkuboko@gmail.com>
2023-05-14 12:25:15 +0200

    commit message

--- profile/src/components/Services.jsx
+++ profile/src/components/Services.jsx
@@ -18,24 +18,24 @@ const Services = () => {
     <div className="flex flex-wrap -m-4">
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">
-          <img className="h-40 rounded w-full object-cover object-center mb-6" alt="hero" src={hero}/>
-          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">Web Devlopment</h3>
+          <img className="h-60 rounded w-full object-cover object-center mb-6" alt="hero" src={hero}/>
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font hover:bg-blue-100 hover:text-black text-large py-2 px-4 border border-gray-400 rounded shadow">Web Devlopment</h3>
           <h2 className="text-lg text-gray-900 font-medium title-font mb-4">FrontEnd</h2>
-          <p className="leading-relaxed text-base">As an excellent frontend developer i craft intuitive, performant interfaces with precision based on client's criteria.</p>
+          <p className="leading-relaxed text-base">I develop intuitive, performant interface frontend websites based on clients criteria.</p>
         </div>
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">
-          <img className="h-40 rounded w-full object-cover object-center mb-6" alt="mac" src={mac}/>
-          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">CyberSecurity</h3>
+          <img className="h-60 rounded w-full object-cover object-center mb-6" alt="mac" src={mac}/>
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font hover:bg-blue-100 hover:text-black text-large py-2 px-4 border border-gray-400 rounded shadow">CyberSecurity</h3>
           <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Web and Server pentesting</h2>
           <p className="leading-relaxed text-base">As an excellent web and server pentester i find and secures vulnerabilities effectively.</p>
         </div>
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">
-          <img className="h-40 rounded w-full object-cover object-center mb-6" alt="web" src={web}/>
-          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font"> Automation & AI</h3>
+          <img className="h-60 rounded w-full object-cover object-center mb-6" alt="web" src={web}/>
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font hover:bg-blue-100 hover:text-black text-large py-2 px-4 border border-gray-400 rounded shadow"> Automation & AI</h3>
           <h2 className="text-lg text-gray-900 font-medium title-font mb-4">AI chatbots</h2>
           <p className="leading-relaxed text-base">ChatGPT
 
@@ -44,8 +44,8 @@ An excellent AI chatbot programmer designs intuitive, responsive conversational 
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">
-          <img className="h-40 rounded w-full object-cover object-center mb-6" alt="mob" src={mob}/>
-          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">WebDevelopment</h3>
+          <img className="h-60 rounded w-full object-cover object-center mb-6" alt="mob" src={mob}/>
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font hover:bg-blue-100 hover:text-black text-large py-2 px-4 border border-gray-400 rounded shadow">WebDevelopment</h3>
           <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Web Design</h2>
           <p className="leading-relaxed text-base">As an Excellent web design i design pleasing, user-friendly, and accessible websites.</p>
         </div>
