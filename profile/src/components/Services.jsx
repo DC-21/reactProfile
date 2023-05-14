b3ca3b0d58639848297ac84052fe45073e8821e4
DC-21 <cholahkuboko@gmail.com>
2023-05-14 11:58:04 +0200

    commit message

--- profile/src/components/Services.jsx
+++ profile/src/components/Services.jsx
@@ -35,16 +35,16 @@ const Services = () => {
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">
           <img className="h-40 rounded w-full object-cover object-center mb-6" alt="web" src={web}/>
-          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Great Pyramid of Giza</h2>
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font"> Automation & AI</h3>
+          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">AI chatbots</h2>
           <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
         </div>
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">
           <img className="h-40 rounded w-full object-cover object-center mb-6" alt="mob" src={mob}/>
-          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">San Francisco</h2>
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">WebDevelopment</h3>
+          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Web Design</h2>
           <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
         </div>
       </div>
