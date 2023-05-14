f3865e8c94581c7d5dcb31effb87d4122435e44f
DC-21 <cholahkuboko@gmail.com>
2023-05-14 11:55:05 +0200

    commit message

--- profile/src/components/Services.jsx
+++ profile/src/components/Services.jsx
@@ -19,16 +19,16 @@ const Services = () => {
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">
           <img className="h-40 rounded w-full object-cover object-center mb-6" alt="hero" src={hero}/>
-          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">Web Devlopment</h3>
+          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">FrontEnd</h2>
           <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
         </div>
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">
           <img className="h-40 rounded w-full object-cover object-center mb-6" alt="mac" src={mac}/>
-          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">CyberSecurity</h3>
+          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Web and Server pentesting</h2>
           <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
         </div>
       </div>
